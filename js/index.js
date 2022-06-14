6e61b64dc7a1dcad96414e0e93348c4b12f592a4
lucioarchimede <larchimede@udesa.edu.ar>
2022-06-13 22:28:09 -0300

    index nuevo sin css

--- js/index.js
+++ js/index.js
@@ -4,41 +4,25 @@ fetch("https://api.allorigins.win/raw?url=https://api.deezer.com/chart")
   })
   .then(function (data) {
 
-    let section = document.querySelector(".bloqueNav");
+    let section = document.querySelector("#bloqueTracks");
     console.log(data);
     let tracks = data.tracks.data
-    for (let i = 0; i < tracks.length; i++) {
-      section.innerHTML += ` <article><img src=${tracks[i].image} alt='' />
-      <p>Name:${tracks[i].name} </p>
-      <p>Status: ${tracks[i].status}</p>  
-      <a href="./detalle.html?id=${tracks[i].id}">ir a detalle</a>
-      </article>`;
-    }
+    for (let i = 0; i < 5; i++) {
+      section.innerHTML += `  <article class="myArticles">
+      <a href="./detail-track.html">
+          <div class="bloque-item-lista">
+              <h1>${tracks[i].title}</h1>
+              <img class="beatles" src="${tracks[i].artist.picture}" alt="foto1">
+              <a href="./detail-track.html?idArtista=${tracks[i].id}"h>Ver detalle</a>
+          </div>
+      </a>
+  </article>`;
+    } 
   })
 .catch(function(error){
     console.log(error);
 })
 
-fetch("https://api.allorigins.win/raw?url=https://api.deezer.com/chart")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-
-    let section = document.querySelector(".bloqueNav");
-    console.log(data);
-    let album = data.albums.data
-    for (let i = 0; i < albums.length; i++) {
-      section.innerHTML += ` <article><img src=${albums[i].image} alt='' />
-      <p>Name:${albums[i].name} </p>
-      <p>Status: ${albums[i].status}</p>  
-      <a href="./detalle.html?id=${albums[i].id}">ir a detalle</a>
-      </article>`;
-    }
-  })
-.catch(function(error){
-    console.log(error);
-})
 
 fetch("https://api.allorigins.win/raw?url=https://api.deezer.com/chart")
   .then(function (response) {
@@ -61,6 +45,30 @@ fetch("https://api.allorigins.win/raw?url=https://api.deezer.com/chart")
   </article>`;
     }
   })
+.catch(function(error){
+    console.log(error);
+})
+
+fetch("https://api.allorigins.win/raw?url=https://api.deezer.com/chart")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+
+    let section = document.querySelector("#bloqueAlbums");
+    console.log(data);
+    let albums = data.albums.data
+    for (let i = 0; i < 5; i++) {
+      section.innerHTML += `<article class="myArticles">
+      <a href="./detail-album.html">
+          <div class="bloque-item-lista">
+              <h1>${albums[i].title}</h1>
+              <img class="beatles" src="${albums[i].cover}" alt="foto1">
+          </div>
+      </a>
+  </article>`; 
+    } 
+  }) 
 .catch(function(error){
     console.log(error);
 })
